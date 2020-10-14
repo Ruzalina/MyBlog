877464cf8f328977146d86cb664e3f1d73c294ac
ruzalina <sulejmanova.ruzalina@bk.ru>
2020-10-14 14:01:47 -0400

    first commit

---
+++ src/main/java/com/ruzalina/blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.ruzalina.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
