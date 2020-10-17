3f1e563b5b87bd6d0f104b424b8eb7964a02ad40
ruzalina <sulejmanova.ruzalina@bk.ru>
2020-10-17 16:15:18 -0400

    jnnbm

--- src/main/java/com/ruzalina/blog/controllers/BlogController.java
+++ src/main/java/com/ruzalina/blog/controllers/BlogController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -22,15 +26,27 @@ public class BlogController {
         return "blog-main";
     }
 
-        @GetMapping("/blog/add")
-        public String blogAdd (Model model){
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model) {
         return "blog-add";
-        }
+    }
 
-        @PostMapping("/blog/add")
-        public String blogPostAdd(@RequestParam String title,@RequestParam String anons,@RequestParam String full_text,Model model){
-        Post post=new Post(title,anons,full_text);
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
+        Post post = new Post(title, anons, full_text);
         postRepository.save(post);
         return "redirect:/blog";
-        }
     }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long id, Model model) {
+        Optional<Post> post = postRepository.findById(id);
+        if (!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-details";
+    }
+}
